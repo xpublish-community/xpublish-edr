09d1a184419637c27b729515e34f5f322613f02b
Matthew Iannucci <matthew@earthmover.io>
2024-11-07 15:56:46 -0500

    Fix bug in serialization of arbitrary time dimensions

--- xpublish_edr/formats/to_geojson.py
+++ xpublish_edr/formats/to_geojson.py
@@ -3,8 +3,19 @@ Generate GeoJSON responses for an xarray dataset for EDR queries
 """
 
 import geopandas as gpd
+import pandas as pd
 import xarray as xr
 from fastapi import Response
+
+
+def handle_date_columns(df: pd.DataFrame) -> pd.DataFrame:
+    """Handle date columns in a GeoDataFrame"""
+    for col in df.columns:
+        if isinstance(df[col].iloc[0], pd.Timestamp):
+            df[col] = df[col].map(lambda x: x.isoformat())
+        elif isinstance(df[col].iloc[0], pd.Timedelta):
+            df[col] = df[col].map(lambda x: x.total_seconds())
+    return df
 
 
 def to_geojson(ds: xr.Dataset):
@@ -13,15 +24,10 @@ def to_geojson(ds: xr.Dataset):
     axes = ds.cf.axes
     (x_col,) = axes["X"]
     (y_col,) = axes["Y"]
-    time_col = axes.get("T")
 
     df = ds.to_dataframe().reset_index()
     gdf = gpd.GeoDataFrame(df, geometry=gpd.points_from_xy(df[x_col], df[y_col]))
-
-    # Map the time to a string if applicable
-    # TODO: Handle timezone?
-    if time_col:
-        gdf[time_col] = gdf[time_col].map(lambda t: t.isoformat())
+    gdf = handle_date_columns(gdf)
 
     json = gdf.to_json()
 
