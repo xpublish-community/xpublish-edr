18bd66841a89ebe17d54247ca283b6e32e434cf9
Matthew Iannucci <matthew@earthmover.io>
2024-11-14 09:12:10 -0500

    Fix indexing so dimensions are never squeezed so they are persisted in response

--- xpublish_edr/geometry/position.py
+++ xpublish_edr/geometry/position.py
@@ -45,9 +45,9 @@ def _select_by_position_regular_xy_grid(
     """
     # Find the nearest X and Y coordinates to the point
     if method == "nearest":
-        return ds.cf.sel(X=point.x, Y=point.y, method=method)
+        return ds.cf.sel(X=[point.x], Y=[point.y], method=method)
     else:
-        return ds.cf.interp(X=point.x, Y=point.y, method=method)
+        return ds.cf.interp(X=[point.x], Y=[point.y], method=method)
 
 
 def _select_by_multiple_positions_regular_xy_grid(
